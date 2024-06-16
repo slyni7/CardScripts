4d84b46f1204e549d3a48695bb28ae167f17b148
slyni <slyni@naver.com>
2024-06-16 23:27:17 +0900

    Update exinit.lua

--- expansions/script/exinit.lua
+++ expansions/script/exinit.lua
@@ -2,6 +2,7 @@ Duel.LoadScript("deprefunc_nodebug.lua")
 
 Duel.LoadScript("proc_version_check.lua")
 
+--[[
 if not Auxiliary.FilterBoolFunctionEx then
 
 	function Auxiliary.FilterBoolFunctionEx(f,...)
@@ -12,6 +13,7 @@ if not Auxiliary.FilterBoolFunctionEx then
 	end
 
 end
+]]--
 
 function Auxiliary.AddEquipProcedure(c,p,f,eqlimit,cost,tg,op,con)
 	local e1=Effect.CreateEffect(c)
