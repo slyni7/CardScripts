c9fde127699cfb178b34b4308ca0dac697fd6037
slyni <slyni@naver.com>
2024-12-08 18:03:00 +0900

    arwer

--- unofficial/c124161183.lua
+++ unofficial/c124161183.lua
@@ -19,6 +19,7 @@ function s.initial_effect(c)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_GRAVE)
 	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(aux.exccon)
 	e2:SetCost(aux.bfgcost)
 	e2:SetTarget(s.tg2)
 	e2:SetOperation(s.op2)
@@ -64,11 +65,11 @@ end
 
 --effect 2
 function s.tg2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetMatchingGroupCount(Card.IsSetCard,tp,LOCATION_GRAVE,0,e:GetHandler(),0xf2b)>0 end
+	if chk==0 then return Duel.GetFlagEffect(tp,124161180)>0 end
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,tp,300)
 end
 
 function s.op2(e,tp,eg,ep,ev,re,r,rp)
-	local ct=Duel.GetMatchingGroupCount(Card.IsSetCard,tp,LOCATION_GRAVE,0,e:GetHandler(),0xf2b)
+	local ct=Duel.GetFlagEffect(tp,124161180)
 	Duel.Damage(1-tp,ct*300,REASON_EFFECT)
 end
