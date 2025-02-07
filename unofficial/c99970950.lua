0084db2b95ec6b4cc46884655bd579ce8ae0ad34
YuL Mk.II <65627621+YuLMk2@users.noreply.github.com>
2025-02-07 16:49:51 -0500

    몽혜룡

--- unofficial/c99970950.lua
+++ unofficial/c99970950.lua
@@ -58,7 +58,7 @@ function s.op1(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetOperation(s.op1op)
 	e1:SetReset(RESET_EVENT|RESETS_STANDARD)
 	c:RegisterEffect(e1,true)
-
+	
 	local cc=Duel.GetCurrentChain()
 	if Duel.SpecialSummonComplete()==1 and cc>1 then
 		local cp=Duel.GetChainInfo(cc-1,CHAININFO_TRIGGERING_PLAYER)
