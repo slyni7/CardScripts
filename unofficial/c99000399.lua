6e5c9ff4d41449f6a2c51ac7634dc1574f3723b2
slyni <slyni@naver.com>
2025-09-22 18:52:52 +0900

    erqwerqwerqwrew

--- unofficial/c99000399.lua
+++ unofficial/c99000399.lua
@@ -1,175 +1,85 @@
---�ÿ� �̸�����Ʈ
-local m=99000399
-local cm=_G["c"..m]
-function cm.initial_effect(c)
-	--order summon
-	aux.AddOrderProcedure(c,"R",nil,cm.ordfil1,aux.FilterBoolFunction(Card.IsSummonType,SUMMON_TYPE_SPECIAL))
-	c:EnableReviveLimit()
-	--att change
+--저 바람에 맞서서 날아올라
+local s,id=GetID()
+function s.initial_effect(c)
+	--이 카드의 발동은 패에서도 할 수 있으며,
+	local e0a=Effect.CreateEffect(c)
+	e0a:SetType(EFFECT_TYPE_SINGLE)
+	e0a:SetCode(EFFECT_TRAP_ACT_IN_HAND)
+	e0a:SetCondition(s.actcon)
+	c:RegisterEffect(e0a)
+	--세트한 턴에도 발동할 수 있다.
+	local e0b=Effect.CreateEffect(c)
+	e0b:SetType(EFFECT_TYPE_SINGLE)
+	e0b:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
+	e0b:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+	e0b:SetCondition(s.actcon)
+	c:RegisterEffect(e0b)
+	--	
+	local e0=Effect.CreateEffect(c)	
+	e0:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e0:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e0:SetCode(EVENT_PREDRAW)
+	e0:SetCountLimit(1,id,EFFECT_COUNT_CODE_DUEL)
+	e0:SetRange(0xf7)
+	e0:SetOperation(s.op)
+	c:RegisterEffect(e0)
+	--덱에서 함정 카드 1장을 패에 넣는다.
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(m,0))
-	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE+EFFECT_FLAG_CANNOT_INACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetHintTiming(TIMING_BATTLE_PHASE)
-	e1:SetCondition(cm.attcon)
-	e1:SetTarget(cm.atttg)
-	e1:SetOperation(cm.attop)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-	--negate
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(m,1))
-	e2:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DISABLE)
-	e2:SetType(EFFECT_TYPE_QUICK_O)
-	e2:SetCode(EVENT_FREE_CHAIN)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
-	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1,m)
-	e2:SetTarget(cm.distg)
-	e2:SetOperation(cm.disop)
-	c:RegisterEffect(e2)
-	--equip
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e3:SetCode(EVENT_BATTLE_DESTROYED)
-	e3:SetCountLimit(1,m+10000)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetCondition(cm.eqcon)
-	e3:SetTarget(cm.eqtg)
-	e3:SetOperation(cm.eqop)
-	c:RegisterEffect(e3)
 end
-cm.CardType_Order=true
-function cm.ordfil1(c)
-	return c:IsSummonType(SUMMON_TYPE_SPECIAL) and c:GetSummonLocation()~=LOCATION_EXTRA
+function s.actcon(e)
+	return Duel.GetTurnCount()~=1
 end
-function cm.attcon(e,tp,eg,ep,ev,re,r,rp)
-	return (Duel.GetCurrentPhase()>=PHASE_BATTLE_START and Duel.GetCurrentPhase()<=PHASE_BATTLE) and not e:GetHandler():IsStatus(STATUS_CHAINING)
-		and (Duel.GetAttacker()==e:GetHandler() or Duel.GetAttackTarget()==e:GetHandler())
-end
-function cm.atttg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():GetFlagEffect(m)==0 end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATTRIBUTE)
-	local aat=Duel.AnnounceAttribute(tp,1,0xff-e:GetHandler():GetAttribute())
-	e:SetLabel(aat)
-	e:GetHandler():RegisterFlagEffect(m,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE,0,1)
-end
-function cm.attop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and c:IsFaceup() then
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_CHANGE_ATTRIBUTE)
-		e1:SetValue(e:GetLabel())
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE)
-		c:RegisterEffect(e1)	
-	end
-	local e2=Effect.CreateEffect(e:GetHandler())
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e2:SetCode(EFFECT_CANNOT_ACTIVATE)
-	e2:SetTargetRange(0,1)
-	e2:SetValue(1)
-	e2:SetReset(RESET_PHASE+PHASE_DAMAGE)
-	Duel.RegisterEffect(e2,tp)
-end
-function cm.disfilter1(c)
-	return c:GetFlagEffect(m+50000)~=0
-end
-function cm.disfilter2(c,rc,tp)
-	if c:IsFaceup() then
-		local att=rc:GetAttribute()
-		for gc in aux.Next(rc:GetEquipGroup():Filter(cm.disfilter1,nil)) do
-			att=att|gc:GetAttribute()
-		end
-		return c:IsFaceup() and c:GetAttribute()&att~=0
+function s.op(e,tp,eg,ep,ev,re,r,rp,chk)
+	if Duel.IsExistingMatchingCard(Card.IsCode,e:GetHandlerPlayer(),0xf7,0,2,nil,id) then
+		Duel.Win(1-tp,0x0)
 	end
 end
-function cm.distg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and cm.disfilter2(chkc,e:GetHandler(),tp) end
-	if chk==0 then return Duel.IsExistingTarget(cm.disfilter2,tp,0,LOCATION_MZONE,1,nil,e:GetHandler(),tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISABLE)
-	Duel.SelectTarget(tp,cm.disfilter2,tp,0,LOCATION_MZONE,1,1,nil,e:GetHandler(),tp)
-end
-function cm.eqfilter(c)
-	return (c:IsFaceup() or c:GetEquipTarget()) and c:IsType(TYPE_EQUIP)
-end
-function cm.disop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_DISABLE)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		tc:RegisterEffect(e1)
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_DISABLE_EFFECT)
-		e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
-		tc:RegisterEffect(e2)
-		local ct=Duel.GetMatchingGroupCount(cm.eqfilter,tp,LOCATION_ONFIELD,0,nil)
-		if ct>0 then
-			local e3=Effect.CreateEffect(c)
-			e3:SetType(EFFECT_TYPE_SINGLE)
-			e3:SetCode(EFFECT_UPDATE_ATTACK)
-			e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-			e3:SetValue(-ct*1000)
-			e3:SetReset(RESET_EVENT+RESETS_STANDARD)
-			tc:RegisterEffect(e3)
+function s.confilter(c)
+	if not (c:IsMonsterCard() and c:IsFaceup()) then return false end
+	local effs={c:GetOwnEffects()}
+	for _,eff in ipairs(effs) do
+		if eff:IsHasType(EFFECT_TYPE_QUICK_O|EFFECT_TYPE_QUICK_F) and eff:GetCode()==EVENT_CHAINING then
+			return true
 		end
 	end
+	return false
 end
-function cm.cfilter(c,rc,tp)
-	return c:IsReason(REASON_BATTLE) and c:IsLocation(LOCATION_GRAVE) and c:GetPreviousControler()==1-tp
-		and c:IsType(TYPE_MONSTER) and c:GetAttribute()&rc:GetAttribute()~=0
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	if chk==0 then return not Duel.HasFlagEffect(tp,id) end
+	return Duel.IsExistingMatchingCard(s.confilter,tp,0,LOCATION_ONFIELD|LOCATION_GRAVE,1,nil)
 end
-function cm.eqcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(cm.cfilter,1,nil,e:GetHandler(),tp)
+function s.filter(c)
+	return c:IsTrap() and c:IsAbleToHand()
 end
-function cm.chkfilter(c,tp)
-	return not c:IsForbidden() and c:CheckUniqueOnField(tp,LOCATION_SZONE)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function cm.filter(c,tp)
-	return c:IsType(TYPE_MONSTER) and c:IsPreviousLocation(LOCATION_MZONE) and c:GetPreviousControler()==1-tp
-		and c:IsLocation(LOCATION_GRAVE) and c:IsReason(REASON_BATTLE) and cm.chkfilter(c,tp)
-end
-function cm.eqtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=eg:Filter(cm.filter,nil,tp)
-	if chk==0 then return #g>0 and Duel.GetLocationCount(tp,LOCATION_SZONE)>=#g end
-	Duel.SetTargetCard(g)
-end
-function cm.eqop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e):Filter(aux.NecroValleyFilter(cm.chkfilter),nil,tp)
-	local ft=Duel.GetLocationCount(tp,LOCATION_SZONE)
-	if c:IsFaceup() and c:IsRelateToEffect(e) and #g>0 and ft>0 then
-		local sg=nil
-		if #g>ft then
-			sg=g:Select(tp,ft,ft,nil)
-		else
-			sg=g
-		end
-		local tc=sg:GetFirst()
-		while tc do
-			if Duel.Equip(tp,tc,c,true,true) then
-				tc:RegisterFlagEffect(m+50000,RESET_EVENT+RESETS_STANDARD,0,0)
-				local e1=Effect.CreateEffect(c)
-				e1:SetType(EFFECT_TYPE_SINGLE)
-				e1:SetCode(EFFECT_EQUIP_LIMIT)
-				e1:SetProperty(EFFECT_FLAG_COPY_INHERIT+EFFECT_FLAG_OWNER_RELATE)
-				e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-				e1:SetValue(cm.eqlimit)
-				tc:RegisterEffect(e1)
-			end
-			tc=sg:GetNext()
-		end
-		Duel.EquipComplete()
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.HasFlagEffect(tp,id) then return end
+	Duel.RegisterFlagEffect(tp,id,0,0,1)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
 	end
-end
-function cm.eqlimit(e,c)
-	return e:GetOwner()==c
+	--이 턴에 1번만, 자신은 함정 카드를 패에서 발동할 수 있다.
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_TRAP_ACT_IN_HAND)
+	e1:SetTargetRange(LOCATION_HAND,0)
+	e1:SetCountLimit(1)
+	e1:SetReset(RESET_PHASE|PHASE_END)
+	Duel.RegisterEffect(e1,tp)
 end
